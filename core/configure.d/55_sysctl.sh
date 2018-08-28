1837efd86ad7ac948c73b33666f0bfdb423e50f4
Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>
2018-08-28 14:28:22 +0200

    core: configure: add HARDEN_FIFO sysctls
    Change-Id: Iccc54812b8cf66659a3c6737c42a46b022e5dfb8

--- core/configure.d/55_sysctl.sh
+++ core/configure.d/55_sysctl.sh
@@ -26,6 +26,8 @@ kernel.pid_max = 65536
 
 fs.protected_symlinks = 1
 fs.protected_hardlinks = 1
+fs.protected_fifos = 2
+fs.protected_regular = 2
 fs.suid_dumpable = 0
 
 net.core.bpf_jit_harden = 2
