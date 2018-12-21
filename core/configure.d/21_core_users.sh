dffda5f1211a2abebaa35f036d930b3fe58769e3
Timothée Ravier <timothee.ravier@ssi.gouv.fr>
2018-12-21 16:41:50 +0100

    core/configure: setup admin & audit users
    Change-Id: I9e3161fa60091806963ab644fc270974b760bb42
    Signed-off-by: Timothée Ravier <timothee.ravier@ssi.gouv.fr>

--- core/configure.d/21_core_users.sh
+++ core/configure.d/21_core_users.sh
@@ -15,11 +15,21 @@ cat <<EOF >> /usr/lib/sysusers.d/clipos-core.conf
 u messagebus 81              "System Message Bus"
 EOF
 
+# admin & audit users
+cat <<EOF >> /usr/lib/sysusers.d/clipos-core.conf
+u admin      800             "Admin user"                        /home/admin /bin/bash
+u audit      801             "Audit user"                        /home/audit /bin/bash
+m audit      systemd-journal
+EOF
+
 # Install sysuser config for the SDK & the final root
 cp {,"${CURRENT_OUT_ROOT}"}/usr/lib/sysusers.d/clipos-core.conf
 
 # Setup for the SDK & the final root
 systemd-sysusers clipos-core.conf
 systemd-sysusers --root="${CURRENT_OUT_ROOT}" clipos-core.conf
+
+# Setup symlinks for home dirs (admin & audit)
+ln -snf "/mnt/state/core/home" "${CURRENT_OUT_ROOT}/home"
 
 # vim: set ts=4 sts=4 sw=4 et ft=sh:
