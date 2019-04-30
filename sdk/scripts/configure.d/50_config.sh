1707e9b0be08154470a4ca1f067f4a4aaaaa7772
Nicolas Godinho <nicolas.godinho@ssi.gouv.fr>
2019-04-30 13:15:58 +0200

    sdk: configure scripts: Revamp locale generation to avoid requiring a shell
    As locale-gen is a Bash script, this commit rewrites the locales
    generation to directly use the binary "localdef" (provided by the
    glibc with the locales) in order to avoid the need to emerge a shell
    interpreter and basic command-line tools (i.e. sed and Awk) in the
    Core rootfs.
    
    This locale generation method also checks that the locales have been
    properly generated and gives a feedback to the CLIP OS developer in
    the configure scripts logs.
    
    Change-Id: I926fe585f40e64db6b32952349b73f38bb42d8dc
    Signed-off-by: Nicolas Godinho <nicolas.godinho@ssi.gouv.fr>

--- sdk/scripts/configure.d/50_config.sh
+++ sdk/scripts/configure.d/50_config.sh
@@ -27,17 +27,53 @@ systemd-firstboot \
     --keymap="${KEYMAP}" \
     --timezone="${TIMEZONE}"
 
-# Add some basic devices to the root tree for the chroot call
+# Split locale into the locale (required for locale generation command line):
+locale_lang='' charmap=''
+IFS=. read -r locale_lang charmap <<< "${LOCALE}"
+
+# Safety checks on locale generation ability:
+if [[ ! -f "${CURRENT_OUT_ROOT}/usr/share/i18n/charmaps/${charmap}.gz" ]]; then
+    sdk_die "Charmap \"${charmap}\" seems absent from root (i.e. in /usr/share/i18n/charmaps)."
+fi
+if [[ ! -f "${CURRENT_OUT_ROOT}/usr/share/i18n/locales/${locale_lang}" ]]; then
+    sdk_die "Locale language \"${locale_lang}\" seems absent from root (i.e. in /usr/share/i18n/locales)."
+fi
+
+sdk_info "Generating locale \"${LOCALE}\"..."
+# Sadly enough, we need to resort to a chroot to generate the required locale
+# within CURRENT_OUT_ROOT. :( Therefore, we need to create some device nodes:
 rm -rf "${CURRENT_OUT_ROOT}/dev"
 install -m 755 -o 0 -g 0 -d "${CURRENT_OUT_ROOT}/dev"
 mknod -m 666 "${CURRENT_OUT_ROOT}/dev/null" c 1 3
+mknod -m 666 "${CURRENT_OUT_ROOT}/dev/full" c 1 7
+mknod -m 666 "${CURRENT_OUT_ROOT}/dev/ptmx" c 5 2
+mknod -m 644 "${CURRENT_OUT_ROOT}/dev/random" c 1 8
+mknod -m 644 "${CURRENT_OUT_ROOT}/dev/urandom" c 1 9
+mknod -m 666 "${CURRENT_OUT_ROOT}/dev/zero" c 1 5
+mknod -m 666 "${CURRENT_OUT_ROOT}/dev/tty" c 5 0
 
-sdk_info "Setup locale"
-echo "${LOCALE} UTF-8" > "${CURRENT_OUT_ROOT}/etc/locale.gen"
-chroot "${CURRENT_OUT_ROOT}" /usr/sbin/locale-gen
+# Do not use the script locale-gen as it requires way too much runtime
+# dependencies that we do not want to add in the Core when non instrumented
+# (i.e. Bash, Awk, sed, etc.):
+env -i chroot "${CURRENT_OUT_ROOT}" /usr/bin/env \
+    localedef -i "${locale_lang}" -f "${charmap}" "${LOCALE}" \
+        || sdk_die "Failure in generating locale \"${LOCALE}\"."
 
-# Cleanup
+available_locales=()
+readarray -t available_locales <<< \
+    "$(env -i chroot "${CURRENT_OUT_ROOT}" /usr/bin/env localedef --list-archive)"
+
+# Cleanup /dev created especially for chroot:
 rm -rf "${CURRENT_OUT_ROOT}/dev"
 install -m 755 -o 0 -g 0 -d "${CURRENT_OUT_ROOT}/dev"
+
+# It is the normal behavior of readarray to create an array with one empty
+# element when the input is empty: filter out this case then:
+if [[ "${#available_locales[@]}" -eq 1 && -z "${available_locales[0]}" ]]; then
+    sdk_die "UNEXPECTED ERROR: List of available locales appears empty even after locale generation."
+fi
+
+sdk_info "Available generated locales:"$'\n'"  ${available_locales[*]}"
+
 
 # vim: set ts=4 sts=4 sw=4 et ft=sh:
