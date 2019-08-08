dae2f6a273ea8bc24a767cf75d8ba01c1b3ec865
Timothée Ravier <timothee.ravier@ssi.gouv.fr>
2019-08-08 15:59:34 +0200

    core: configure: add updater configuration
    Change-Id: I8cf1fa602d152929a88b74f9dc4c416b5456d796
    Signed-off-by: Timothée Ravier <timothee.ravier@ssi.gouv.fr>

---
+++ core/configure.d/58_update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# Copyright © 2019 ANSSI. All rights reserved.
+
+# Safety settings: do not remove!
+set -o errexit -o nounset -o pipefail
+
+# The prelude to every script for this SDK. Do not remove it.
+source /mnt/products/${CURRENT_SDK_PRODUCT}/${CURRENT_SDK_RECIPE}/scripts/prelude.sh
+
+sdk_info "Enable updater timer by default"
+systemctl --root="${CURRENT_OUT_ROOT}" enable updater.timer
+
+# Remote configuration is stored in the state partition to allow changes by
+# admin users
+ln -snf "/mnt/state/core/etc/updater" "${CURRENT_OUT_ROOT}/etc/updater"
+
+# System configuration is stored in the RO / partition
+sdk_info "Install static system configuration"
+
+cat <<EOF > "${CURRENT_OUT_ROOT}/usr/lib/updater/config.toml"
+os_name = "clipos"
+
+[core]
+destination = "mainvg"
+size = "4G"
+
+[efiboot]
+destination = "/mnt/efiboot/EFI/Linux"
+EOF
+
+cat <<EOF > "${CURRENT_OUT_ROOT}/usr/lib/updater/pubkey"
+untrusted comment: minisign public key: 70D830FF14CDFCC9
+RWTJ/M0U/zDYcGXzF2FC3fsz/PgZUs3PFI4Co3Ul/2udRk6PCde+B++S
+EOF
+
+# vim: set ts=4 sts=4 sw=4 et ft=sh:
