b908844b0dc7a6dc83fe7ce9a96f09b31e140476
Nicolas Bouchinet <nicolas.bouchinet@clip-os.org>
2020-11-13 10:23:25 +0100

    sdk/configure: Fix rootfs copy
    Treat DEST as a normal file to avoid copy encapsulation
    if the DEST root directory already exists.
    
    Signed-off-by: Nicolas Bouchinet <nicolas.bouchinet@clip-os.org>
    Change-Id: I23ad12e1b8f79e744c4e5e248d786456f4b3f5c0

--- sdk/configure.d/00_import_rootfs.sh
+++ sdk/configure.d/00_import_rootfs.sh
@@ -8,6 +8,6 @@ set -o errexit -o nounset -o pipefail
 # The prelude to every script for this SDK. Do not remove it.
 source /mnt/products/${COSMK_SDK_PRODUCT}/${COSMK_SDK_RECIPE}/prelude.sh
 
-cp -a /mnt/out/${COSMK_PRODUCT}/${COSMK_PRODUCT_VERSION}/${COSMK_RECIPE}/image/root ${CURRENT_OUT_ROOT}
+cp -a -T /mnt/out/${COSMK_PRODUCT}/${COSMK_PRODUCT_VERSION}/${COSMK_RECIPE}/image/root ${CURRENT_OUT_ROOT}
 
 # vim: set ts=4 sts=4 sw=4 et ft=sh:
